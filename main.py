3ecf1fa46865a3da58bc3e301e725614851dbd62
Kseniya Zakirova <ksemiya@gmail.com>
2023-07-31 16:56:22 +0200

    add rule 16

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import time
 import today_rules  # I should change that but idk how
 import passwordDriverWrapper
 import re
+import requests
 import pandas as pd
+from xml.etree import ElementTree as ET
 
 THRESHOLD = 1  # magic int; I think it will be enough degree of freedom
 
@@ -62,6 +64,107 @@ def captcha_solver(driver: passwordDriverWrapper.PasswordDriverWrapper):
     return captcha, captcha_digit_sum
 
 
+def get_chess_position(chess_img):
+    # Make a request to the URL
+    response = requests.get(chess_img)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        svg_data = response.content
+
+        # Parse SVG data
+        tree = ET.ElementTree(ET.fromstring(svg_data))
+
+        # Loop over all elements in the SVG
+        for elem in tree.iter():
+            if elem.text is not None:
+                chess_position = elem.text
+                return chess_position
+    else:
+        print("Failed to fetch SVG.")  # TODO raise Err
+    return
+
+
+def color_move(chess_move):
+    if "White" in chess_move:
+        return "w"
+    if "Black" in chess_move:
+        return "b"
+    return "WRONG MOVE"
+
+
+def from_svg_to_FEN(chess_position, chess_move):
+    # Remove leading/trailing white space and split the string into lines
+    rows = chess_position.strip().split("\n")
+
+    # Initialize an empty list to hold FEN rows
+    fen_rows = []
+
+    # Iterate over rows
+    for row in rows:
+        # Remove leading/trailing white space and split row into squares
+        squares = row.strip().split(" ")
+        # Initialize counter for empty squares
+        empty = 0
+        # Initialize an empty string to hold FEN for this row
+        fen_row = ""
+        # Iterate over squares
+        for square in squares:
+            if square == ".":
+                # Increment counter for empty squares
+                empty += 1
+            else:
+                # If the square is not empty and there were empty squares before, add count to FEN
+                if empty > 0:
+                    fen_row += str(empty)
+                    # Reset empty squares counter
+                    empty = 0
+                # Add piece to FEN
+                fen_row += square
+        # If the row ends with one or more empty squares, add count to FEN
+        if empty > 0:
+            fen_row += str(empty)
+        # Add FEN for this row to the list of FEN rows
+        fen_rows.append(fen_row)
+    # Join the FEN rows with slashes to get the final FEN
+    fen_board = "/".join(fen_rows)
+    fen = fen_board + " " + color_move(chess_move) + " - - 0 1"
+
+    return fen
+
+
+def get_best_move(fen):
+    import chess.engine
+
+    # Define the path to your Stockfish engine file
+    engine_path = "/opt/homebrew/Cellar/stockfish/16/bin/stockfish"
+
+    # Initialize the chessboard with a given FEN
+    board = chess.Board(fen)
+
+    # Initialize the engine
+    engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+
+    # Find the best move
+    result = engine.play(board, chess.engine.Limit(time=1.0))
+    best_move = result.move
+
+    # From UCI to algebraic notation
+    best_move_alg_not = board.san(best_move)
+
+    # It is good practice to close the engine after using it
+    engine.quit()
+
+    return best_move_alg_not
+
+
+def solve_chess_position(chess_img, chess_move):
+    chess_position = get_chess_position(chess_img)
+    fen = from_svg_to_FEN(chess_position, chess_move)
+    best_move = get_best_move(fen)
+    return best_move
+
+
 def main():
     free_digit = 25
     first_password = (
@@ -91,7 +194,19 @@ def main():
         countiers[countiers.embed == geo_embed].title.values[0].lower().replace(" ", "")
     )
     password = password + str_to_password(country)
-    return
+    # rule 16
+    chess_img = driver.get_chess_svg
+    chess_move = driver.get_chess_move_text
+    chess_solution = solve_chess_position(chess_img, chess_move)
+    chess_digit_sum = sum([int(x) for x in re.findall(r"\d", chess_solution)])
+    free_digit = free_digit - chess_digit_sum
+    password = (
+        str_to_password("🥚")
+        + (password[chess_digit_sum:] if chess_digit_sum != 0 else password)
+        + str_to_password(chess_solution)
+    )
+
+    driver.update_password(password_to_str(password))
 
 
 if __name__ == "__main__":
